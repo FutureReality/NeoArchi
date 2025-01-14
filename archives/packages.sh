53f6acc03ca61bb80f719ac7d8083e0a275e7313
FutureReality <115637305+FutureReality@users.noreply.github.com>
2025-01-14 23:11:08 +0100

    Create packages.sh

---
+++ archives/packages.sh
@@ -0,0 +1,14 @@
+#Paquetes relacionados con el entorno grafico
+pacman -Syu qtile polybar lightdm lightdm-gtk-greeter lightdm-webkit2-greeter arandr
+
+#Entorno grafico no-esencial
+pacman -Syu kitty nitrogen dunst rofi picom
+
+#Servicios necesarios
+pacman -Syu pulseaudio networkmanager cups
+
+#paquetes basicos para archivos
+pacman -Syu nano vim feh mpv
+
+#paquetes para analisis del sistema y de red
+pacman -Syu neofetch bottom 
