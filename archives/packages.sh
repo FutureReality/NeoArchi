0099014286472f11836f71f636a7a72c740598b7
FutureReality <115637305+FutureReality@users.noreply.github.com>
2025-01-15 10:16:41 +0100

    Update packages.sh

--- archives/packages.sh
+++ archives/packages.sh
@@ -4,11 +4,23 @@ pacman -Syu qtile polybar lightdm lightdm-gtk-greeter lightdm-webkit2-greeter ar
 #Entorno grafico no-esencial
 pacman -Syu kitty nitrogen dunst rofi picom
 
-#Servicios necesarios
-pacman -Syu pulseaudio networkmanager cups
+#Servicios necesarios y utilidades del sistema
+pacman -Syu pulseaudio networkmanager cups alsa-utils usbutils net-tools util-linux build-essential
 
 #paquetes basicos para archivos
-pacman -Syu nano vim feh mpv
+pacman -Syu nano vim feh mpv unrar unzip p7zip gzip
 
 #paquetes para analisis del sistema y de red
-pacman -Syu neofetch bottom 
+pacman -Syu neofetch bottom iftop tcpdump curl wget sysstat lm-sensor
+
+#paquetes de seguridad
+pacman -Syu ufw iptables pass fail2ban openssl logwatch
+
+#Ofimatica y variado
+pacman -Syu firefox virtualbox git
+
+#Lenguajes
+pacman -Syu python3 python-pip jdk-openjdk php mysql
+
+#paquetes opcionales
+pacman -Syu qutebrowser neovim libreoffice mpd ncmpcpp
